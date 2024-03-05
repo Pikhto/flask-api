3dd1bfe67f7015086e76892b51c6b2431fc60fce
Andrey Kuzmin <andromed-ku@yandex.ru>
2024-03-05 22:19:26 +0500

    Update file

--- fkcommerce/core/__init__.py
+++ fkcommerce/core/__init__.py
@@ -1,10 +1,23 @@
+import os
+
 from dotenv import load_dotenv
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
 
+database = SQLAlchemy()
 
-def create_app():
+
+def create_app(config_type=os.getenv("CONFIG_TYPE")):
     app = Flask(__name__)
 
+    app.config.from_object(config_type)
+
+    initialize_extensions(app)
+
     return app
+
+
+def initialize_extensions(app):
+    database.init_app(app)
