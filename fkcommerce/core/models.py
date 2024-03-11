095afdcb447c744c3da8adb1dfee0041173aa2c5
Andrey Kuzmin <andromed-ku@yandex.ru>
2024-03-11 21:05:27 +0500

    Update file

--- fkcommerce/core/models.py
+++ fkcommerce/core/models.py
@@ -1,6 +1,5 @@
 import uuid
 
-from core import database as db
 from sqlalchemy import (
     DECIMAL,
     Boolean,
@@ -14,6 +13,21 @@ from sqlalchemy import (
     text,
 )
 from sqlalchemy.dialects.postgresql import UUID
+
+from core import database as db
+
+
+class Category(db.Model):
+    __tablename__ = "category"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), unique=True, nullable=False)
+    slug = Column(String(200), unique=True, nullable=False)
+    is_active = Column(Boolean, default=False)
+    parent_id = Column(Integer, ForeignKey("category.id"))
+
+    def __repr__(self) -> str:
+        return f"<Name: {self.name}>"
 
 
 class Product(db.Model):
@@ -83,3 +97,52 @@ class SeasonalEvent(db.Model):
 
     def __repr__(self):
         return self.name
+
+
+class Attribute(db.Model):
+    __tablename__ = "attribute"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200))
+    description = Column(Text)
+
+    def __repr__(self):
+        return self.name
+
+
+class AttributeValue(db.Model):
+    __tablename__ = "attribute_value"
+
+    id = Column(Integer, primary_key=True)
+    attribute_value = Column(String(100))
+    attribute_id = Column(Integer, ForeignKey("attribute.id"))
+
+    def __repr__(self):
+        return self.attribute_value
+
+
+class ProductType(db.Model):
+    __tablename__ = "product_type"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    parent_id = Column(Integer, ForeignKey("product_type.id"))
+
+    def __repr__(self):
+        return self.name
+
+
+class Product_ProductType(db.Model):
+    __tablename__ = "product_product_type"
+
+    id = Column(Integer, primary_key=True)
+    product_id = Column(Integer, ForeignKey("product.id"))
+    product_type_id = Column(Integer, ForeignKey("product_type.id"))
+
+
+class ProductLine_AttributeValue(db.Model):
+    __tablename__ = "product_line_attribute_value"
+
+    id = Column(Integer, primary_key=True)
+    attribute_value_id = Column(Integer, ForeignKey("attribute_value.id"))
+    product_line_id = Column(Integer, ForeignKey("product_line.id"))
