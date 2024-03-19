f874782c3c0f33df5a5e52548ac232b121a99a19
Andrey Kuzmin <andromed-ku@yandex.ru>
2024-03-19 23:55:46 +0500

    Update file

--- fkcommerce/core/__init__.py
+++ fkcommerce/core/__init__.py
@@ -1,7 +1,9 @@
 import os
 
+from apifairy import APIFairy
 from dotenv import load_dotenv
 from flask import Flask
+from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -11,6 +13,10 @@ database = SQLAlchemy()
 
 db_migration = Migrate()
 
+ma = Marshmallow()
+
+api_fairy = APIFairy()
+
 
 def create_app(config_type=os.getenv("CONFIG_TYPE")):
     app = Flask(__name__)
@@ -18,6 +24,8 @@ def create_app(config_type=os.getenv("CONFIG_TYPE")):
     app.config.from_object(config_type)
 
     initialize_extensions(app)
+
+    register_blueprint(app)
 
     return app
 
@@ -27,4 +35,14 @@ def initialize_extensions(app):
 
     db_migration.init_app(app, database)
 
+    ma.init_app(app)
+
+    api_fairy.init_app(app)
+
     import core.models  # noqa: F401
+
+
+def register_blueprint(app: Flask):
+    from core.inventory_api import inventory_category_api_blueprint
+
+    app.register_blueprint(inventory_category_api_blueprint, url_prefix="/api")
